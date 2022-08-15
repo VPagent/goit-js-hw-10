7ab19249f28c889528c49aba3f628547ecb96813
VPagent <vpagent13@gmail.com>
2022-08-15 19:05:20 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+const list = document.querySelector(".country-list")
+const divInfo = document.querySelector(".country-info")
+const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector("#search-box")
+
+input.addEventListener("input", debounce(onInputValue, DEBOUNCE_DELAY))
+
+function onInputValue(event){
+    let inputValue = event.target.value
+    if(!inputValue){
+        return        
+    }
+    fetchCountries(inputValue).then(json => {
+        let markup = ""
+        
+        if(inputValue.length <= 1){
+            list.innerHTML = ""
+            return Notiflix.Notify.info
+            ("Too many matches found. Please enter a more specific name.")
+        }
+        if(json.length >= 2 && json.length <= 10){
+            markup =""
+            json.map(elem => {     
+                markup += `<li class="item">
+                <img src="${elem.flags.svg}" alt="${elem.name.official}" width="50px" height="30px"/>
+                <h2 class="text">${elem.name.common}</h2>
+              </li> `
+                list.innerHTML = markup
+            });
+        }
+        if(json.length === 1){
+            list.innerHTML = ""
+            markup= ""
+            // console.log(json)
+            json.forEach(elem => {
+                let languages = Object.values(elem.languages).join(", ")
+                markup = `<h2><span class="flag"><img src="${elem.flags.svg}" alt="${elem.name.official}" width="50px" height="30px"></span>${elem.name.common}</h2>
+                <p><span class="big">Capital:</span>${elem.capital[0]}</p>
+                <p><span class="big">Population:</span>${elem.population}</p>
+                <p><span class="big">Languages:</span>${languages}</p>`
+                divInfo.innerHTML = markup
+            })
+        }
+        if(!json.status.ok){
+            list.innerHTML = ""
+            return Notiflix.Notify.failure("Oops, there is no country with that name")
+        }
+        
+        
+    })
+    
+}
+
+
+        // allCountry.name = elem.name.official
+                // allCountry.capital = elem.capital[0]
+                // allCountry.population = elem.population
+                // allCountry.flag = elem.flags.png
+                // allCountry.languages = Object.values(elem.languages).join(", ")
+                // console.log(elem.name)
+
+
+
+    
